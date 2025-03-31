76a36c056a91b62b059a92824974b38843aca7ee
James Prestwich <james@prestwi.ch>
2025-03-31 06:48:35 -0400

    feat: serve router over ws (#26)
    * feat: serve router over ws
    
    * fix: don't bind
    
    * chore: cleanup
    
    * chore: port RpcServerGuard
    
    * feat: from rpcserverargs
    
    * fix: serve ws at /
    
    * fix: respect WS cors

--- crates/rpc/src/lib.rs
+++ crates/rpc/src/lib.rs
@@ -12,7 +12,7 @@
 //! # use signet_rpc::{Pnt, RpcCtx};
 //! # use reth_node_api::FullNodeComponents;
 //! # use reth::tasks::TaskExecutor;
-//! use signet_rpc::{router, serve_axum};
+//! use signet_rpc::{router, ServeConfig};
 //!
 //! # pub async fn f<Host, Signet>(ctx: RpcCtx<Host, Signet>, tasks: &TaskExecutor) -> eyre::Result<()>
 //! # where
@@ -20,13 +20,18 @@
 //! #   Signet: Pnt,
 //! # {
 //! let router = signet_rpc::router().with_state(ctx);
-//! // Spawn the server on the given addresses.
-//! let _ = serve_axum(
-//!     tasks,
-//!     &router,
-//!     &["localhost:8080".parse()?],
-//!     None
-//! ).await?;
+//!
+//! let cfg = ServeConfig {
+//!     http: vec!["localhost:8080".parse()?],
+//!     http_cors: None,
+//!     ws: vec![],
+//!     ws_cors: None,
+//!     ipc: None,
+//! };
+//!
+//! // Spawn the server on the given addresses, the shutdown guard
+//! // will shutdown the server(s) when dropped.
+//! let shutdown_guard = cfg.serve(tasks, router).await?;
 //! # Ok(())
 //! # }
 //! ```
@@ -43,6 +48,9 @@
 #![deny(unused_must_use, rust_2018_idioms)]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 
+mod config;
+pub use config::{RpcServerGuard, ServeConfig};
+
 mod ctx;
 pub use ctx::RpcCtx;
 
@@ -57,24 +65,15 @@ mod interest;
 mod forwarder;
 use forwarder::TxCacheForwarder;
 
-pub(crate) mod util;
+/// Utils and simple serve functions.
+pub mod util;
 pub use util::Pnt;
 
 /// Re-exported for convenience
 pub use ::ajj;
 
-use ajj::{
-    pubsub::{Connect, ServerShutdown},
-    Router,
-};
-use axum::http::{HeaderValue, Method};
-use interprocess::local_socket as ls;
-use reth::tasks::TaskExecutor;
+use ajj::Router;
 use reth_node_api::FullNodeComponents;
-use std::{future::IntoFuture, net::SocketAddr};
-use tokio::task::JoinHandle;
-use tower_http::cors::{AllowOrigin, CorsLayer};
-use tracing::error;
 
 /// Create a new router with the given host and signet types.
 pub fn router<Host, Signet>() -> Router<ctx::RpcCtx<Host, Signet>>
@@ -83,61 +82,4 @@ where
     Signet: Pnt,
 {
     ajj::Router::new().nest("eth", eth::<Host, Signet>()).nest("signet", signet::<Host, Signet>())
-}
-
-/// Serve the router on the given addresses using axum.
-pub async fn serve_axum(
-    tasks: &TaskExecutor,
-    router: &Router<()>,
-    addrs: &[SocketAddr],
-    cors: Option<&str>,
-) -> eyre::Result<JoinHandle<()>> {
-    let cors = cors
-        .unwrap_or("*")
-        .parse::<HeaderValue>()
-        .map(Into::<AllowOrigin>::into)
-        .unwrap_or_else(|_| AllowOrigin::any());
-
-    let cors = CorsLayer::new().allow_methods([Method::GET, Method::POST]).allow_origin(cors);
-
-    let service = router.clone().into_axum("/").layer(cors);
-
-    let listener = tokio::net::TcpListener::bind(addrs).await?;
-
-    let fut = async move {
-        match axum::serve(listener, service).into_future().await {
-            Ok(_) => (),
-            Err(err) => error!(%err, "Error serving RPC via axum"),
-        }
-    };
-
-    Ok(tasks.spawn(fut))
-}
-
-/// Serve the router on the given address using a Websocket.
-pub const fn serve_ws(_tasks: &TaskExecutor, _router: &Router<()>, _addr: SocketAddr) {
-    // TODO: ENG-826
-}
-
-fn to_name(path: &std::ffi::OsStr) -> std::io::Result<ls::Name<'_>> {
-    if cfg!(windows) && !path.as_encoded_bytes().starts_with(br"\\.\pipe\") {
-        ls::ToNsName::to_ns_name::<ls::GenericNamespaced>(path)
-    } else {
-        ls::ToFsName::to_fs_name::<ls::GenericFilePath>(path)
-    }
-}
-
-/// Serve the router on the given address using IPC.
-pub async fn serve_ipc(
-    tasks: &TaskExecutor,
-    router: &Router<()>,
-    endpoint: &str,
-) -> eyre::Result<ServerShutdown> {
-    let name = std::ffi::OsStr::new(endpoint);
-    let name = to_name(name).expect("invalid name");
-    ls::ListenerOptions::new()
-        .name(name)
-        .serve_with_handle(router.clone(), tasks.handle().clone())
-        .await
-        .map_err(Into::into)
 }
