28ee82295ebd39462739446d703de295dcecdd6c
Anna Carroll <anna.s.carroll@gmail.com>
2025-04-16 16:09:25 -0400

    add docs to permit2 consts

--- crates/zenith/src/bindings.rs
+++ crates/zenith/src/bindings.rs
@@ -5,7 +5,9 @@ use alloy::{
     sol_types::{Eip712Domain, SolStruct},
 };
 
+/// Name of the Permit2 contract, used in EIP-712 domain.
 const PERMIT2_CONTRACT_NAME: &str = "Permit2";
+/// Address of the Permit2 contract, used in EIP-712 domain. Permit2 is CREATE2 deployed at the same address on every chain.
 const PERMIT2_ADDRESS: Address = address!("0x000000000022D473030F116dDEE9F6B43aC78BA3");
 
 mod mint {
