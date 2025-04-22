726ea61892c430b9602f3c8b3ed39a25a23472a6
James <james@prestwi.ch>
2025-04-22 16:09:01 -0400

    docs: more of em

--- crates/sim/src/outcome.rs
+++ crates/sim/src/outcome.rs
@@ -25,7 +25,13 @@ pub struct SimOutcomeWithCache {
 /// An item after simulation, containing the score and gas used.
 #[derive(Debug, Clone)]
 pub struct SimulatedItem {
+    /// The score of the simulation, a [`U256`] value that represents the
+    /// increase in the beneficiary's balance.
     pub score: U256,
+
+    /// The total amount of gas used by the simulation.
     pub gas_used: u64,
+
+    /// The transaction or bundle that was simulated.
     pub item: SimItem,
 }
