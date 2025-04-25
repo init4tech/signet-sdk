6f47d35a442b802e99a2269bc1d8f1e8d1170407
Anna Carroll <anna.s.carroll@gmail.com>
2025-04-25 17:19:35 +0100

    update comment

--- crates/zenith/src/orders/signing/order.rs
+++ crates/zenith/src/orders/signing/order.rs
@@ -52,7 +52,7 @@ impl SignedOrder {
 /// An UnsignedOrder is a helper type used to easily transform an Order into a SignedOrder with correct permit2 semantics.
 /// Users can do:
 /// let signed_order = UnsignedOrder::from(order).with_chain(rollup_chain_id, rollup_order_address).sign(signer)?;
-/// TxCacheForwarder::new(tx_cache_endpoint).send_order(signed_order);
+/// TxCache::new(tx_cache_endpoint).forward_order(signed_order);
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
 pub struct UnsignedOrder<'a> {
     order: Cow<'a, Order>,
