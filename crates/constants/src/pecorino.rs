7aab71b2b33f6a6812b901311ac324d16ef4e73b
James <james@prestwi.ch>
2025-05-08 13:16:50 -0400

    chore: make test constants conform

--- crates/constants/src/pecorino.rs
+++ crates/constants/src/pecorino.rs
@@ -40,15 +40,14 @@ pub const RU_PASSAGE: Address = todo!();
 pub const BASE_FEE_RECIPIENT: Address = todo!();
 
 /// Host system tokens for Pecorino.
-pub const HOST_TOKENS: crate::PredeployTokens =
+pub const HOST_TOKENS: PredeployTokens =
     crate::PredeployTokens::new(HOST_USDC, HOST_USDT, HOST_WBTC);
 
 /// RU system tokens for Pecorino.
-pub const RU_TOKENS: crate::PredeployTokens =
-    crate::PredeployTokens::new(RU_USDC, RU_USDT, RU_WBTC);
+pub const RU_TOKENS: PredeployTokens = crate::PredeployTokens::new(RU_USDC, RU_USDT, RU_WBTC);
 
 /// Host system constants for Pecorino.
-pub const HOST: crate::HostConstants = crate::HostConstants::new(
+pub const HOST: HostConstants = crate::HostConstants::new(
     HOST_ID,
     DEPLOY_HEIGHT,
     HOST_ZENITH,
@@ -59,7 +58,7 @@ pub const HOST: crate::HostConstants = crate::HostConstants::new(
 );
 
 /// RU system constants for Pecorino.
-pub const ROLLUP: crate::RollupConstants =
+pub const ROLLUP: RollupConstants =
     crate::RollupConstants::new(ROLLUP_ID, RU_ORDERS, RU_PASSAGE, BASE_FEE_RECIPIENT, RU_TOKENS);
 
 /// Signet system constants for Pecorino.
--- crates/constants/src/test_utils.rs
+++ crates/constants/src/test_utils.rs
@@ -5,64 +5,61 @@ use alloy::primitives::Address;
 pub const DEFAULT_REWARD_ADDRESS: Address = Address::repeat_byte(0x81);
 
 /// Test chain id for the host chain.
-pub const TEST_HOST_CHAIN_ID: u64 = 1;
+pub const HOST_ID: u64 = 1;
+/// Test deployment height.
+pub const DEPLOY_HEIGHT: u64 = 0;
+/// Test address for the host zenith.
+pub const HOST_ZENITH: Address = Address::repeat_byte(0x82);
+/// Test address for the host orders.
+pub const HOST_ORDERS: Address = Address::repeat_byte(0x83);
+/// Test address for the host passage.
+pub const HOST_PASSAGE: Address = Address::repeat_byte(0x84);
+/// Test address for the host transactor
+pub const HOST_TRANSACTOR: Address = Address::repeat_byte(0x85);
+
+/// Test address for predeployed USDC
+pub const HOST_USDC: Address = Address::repeat_byte(0x89);
+/// Test address for predeployed USDT
+pub const HOST_USDT: Address = Address::repeat_byte(0x8a);
+/// Test address for predeployed WBTC
+pub const HOST_WBTC: Address = Address::repeat_byte(0x8b);
+
+/// Test address for predeployed USDC
+pub const RU_USDC: Address = Address::repeat_byte(0x89);
+/// Test address for predeployed USDT
+pub const RU_USDT: Address = Address::repeat_byte(0x8a);
+/// Test address for predeployed WBTC
+pub const RU_WBTC: Address = Address::repeat_byte(0x8b);
 
 /// Test chain id for the RU chain.
-pub const TEST_RU_CHAIN_ID: u64 = 15;
-
-/// Test address for the host zenith.
-pub const HOST_ZENITH_ADDRESS: Address = Address::repeat_byte(0x82);
-
-/// Test address for the host orders.
-pub const HOST_ORDERS_ADDRESS: Address = Address::repeat_byte(0x83);
-
-/// Test address for the host passage.
-pub const HOST_PASSAGE_ADDRESS: Address = Address::repeat_byte(0x84);
-
-/// Test address for the host transactor
-pub const HOST_TRANSACTOR_ADDRESS: Address = Address::repeat_byte(0x85);
-
+pub const ROLLUP_ID: u64 = 15;
 /// Test address for the RU zenith.
-pub const RU_ORDERS_ADDRESS: Address = Address::repeat_byte(0x86);
-
+pub const RU_ORDERS: Address = Address::repeat_byte(0x86);
 /// Test address for the RU passage.
-pub const RU_PASSAGE_ADDRESS: Address = Address::repeat_byte(0x87);
-
+pub const RU_PASSAGE: Address = Address::repeat_byte(0x87);
 /// Test address for the base fee recipient.
 pub const BASE_FEE_RECIPIENT: Address = Address::repeat_byte(0x88);
 
-/// Test address for predeployed USDC
-pub const USDC: Address = Address::repeat_byte(0x89);
+/// Host system tokens for Pecorino.
+pub const HOST_TOKENS: PredeployTokens = PredeployTokens::new(HOST_USDC, HOST_USDT, HOST_WBTC);
 
-/// Test address for predeployed USDT
-pub const USDT: Address = Address::repeat_byte(0x8a);
-
-/// Test address for predeployed WBTC
-pub const WBTC: Address = Address::repeat_byte(0x8b);
-
-/// Predeployed tokens for testing
-pub const TEST_PREDEPLOYS: PredeployTokens = PredeployTokens::new(USDC, USDT, WBTC);
+/// RU system tokens for Pecorino.
+pub const RU_TOKENS: PredeployTokens = PredeployTokens::new(RU_USDC, RU_USDT, RU_WBTC);
 
 /// Host config
-pub const TEST_HOST_CONFIG: HostConstants = HostConstants::new(
-    TEST_HOST_CHAIN_ID,
+pub const HOST: HostConstants = HostConstants::new(
+    HOST_ID,
     0,
-    HOST_ZENITH_ADDRESS,
-    HOST_ORDERS_ADDRESS,
-    HOST_PASSAGE_ADDRESS,
-    HOST_TRANSACTOR_ADDRESS,
-    TEST_PREDEPLOYS,
+    HOST_ZENITH,
+    HOST_ORDERS,
+    HOST_PASSAGE,
+    HOST_TRANSACTOR,
+    HOST_TOKENS,
 );
 
 /// Rollup config
-pub const TEST_ROLLUP_CONFIG: RollupConstants = RollupConstants::new(
-    TEST_RU_CHAIN_ID,
-    RU_ORDERS_ADDRESS,
-    RU_PASSAGE_ADDRESS,
-    BASE_FEE_RECIPIENT,
-    TEST_PREDEPLOYS,
-);
+pub const ROLLUP: RollupConstants =
+    RollupConstants::new(ROLLUP_ID, RU_ORDERS, RU_PASSAGE, BASE_FEE_RECIPIENT, RU_TOKENS);
 
 /// Test constants for unit tests.
-pub const TEST_CONSTANTS: SignetSystemConstants =
-    SignetSystemConstants::new(TEST_HOST_CONFIG, TEST_ROLLUP_CONFIG);
+pub const TEST_CONSTANTS: SignetSystemConstants = SignetSystemConstants::new(HOST, ROLLUP);
