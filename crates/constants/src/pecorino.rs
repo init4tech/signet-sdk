03d14523023d6870c729822ea4e2f93ecb4c5b38
evalir <e@evalir.xyz>
2025-05-08 19:35:50 +0200

    chore: add pecorino constants

--- crates/constants/src/pecorino.rs
+++ crates/constants/src/pecorino.rs
@@ -1,43 +1,43 @@
 //! Constants for the Pecorino testnet.
 
 use crate::{HostConstants, PredeployTokens, RollupConstants, SignetSystemConstants};
-use alloy::primitives::Address;
+use alloy::primitives::{address, Address};
 
 /// Chain ID for the Pecorino testnet host chain.
-pub const HOST_ID: u64 = todo!();
+pub const HOST_ID: u64 = 3151908;
 /// Deployment height for the Pecorino testnet host chain.
-pub const DEPLOY_HEIGHT: u64 = todo!();
+pub const DEPLOY_HEIGHT: u64 = 149984; 
 /// `Zenith` contract address for the Pecorino testnet host chain.
-pub const HOST_ZENITH: Address = todo!();
+pub const HOST_ZENITH: Address = address!("0xbe45611502116387211D28cE493D6Fb3d192bc4E");
 /// `Orders` contract address for the Pecorino testnet host chain.
-pub const HOST_ORDERS: Address = todo!();
+pub const HOST_ORDERS: Address = address!("0x4E8cC181805aFC307C83298242271142b8e2f249");
 /// `Passage` contract address for the Pecorino testnet host chain.
-pub const HOST_PASSAGE: Address = todo!();
+pub const HOST_PASSAGE: Address = address!("0xd553C4CA4792Af71F4B61231409eaB321c1Dd2Ce");
 /// `Transactor` contract address for the Pecorino testnet host chain.
-pub const HOST_TRANSACTOR: Address = todo!();
+pub const HOST_TRANSACTOR: Address = address!("0x1af3A16857C28917Ab2C4c78Be099fF251669200");
 
 /// USDC token for the Pecorino testnet host chain.
-pub const HOST_USDC: Address = todo!();
+pub const HOST_USDC: Address = address!("0x885F8DB528dC8a38aA3DDad9D3F619746B4a6A81");
 /// USDT token for the Pecorino testnet host chain.
-pub const HOST_USDT: Address = todo!();
+pub const HOST_USDT: Address = address!("0x7970D259D4a96764Fa9B23FF0715A35f06f52D1A");
 /// WBTC token for the Pecorino testnet host chain.
-pub const HOST_WBTC: Address = todo!();
+pub const HOST_WBTC: Address = address!("0x9aeDED4224f3dD31aD8A0B1FcD05E2d7829283a7");
 
 /// USDC token for the Pecorino testnet RU chain.
-pub const RU_USDC: Address = todo!();
+pub const RU_USDC: Address = address!("0x0B8BC5e60EE10957E0d1A0d95598fA63E65605e2");
 /// USDT token for the Pecorino testnet RU chain.
-pub const RU_USDT: Address = todo!();
+pub const RU_USDT: Address = address!("0xF34326d3521F1b07d1aa63729cB14A372f8A737C");
 /// WBTC token for the Pecorino testnet RU chain.
-pub const RU_WBTC: Address = todo!();
+pub const RU_WBTC: Address = address!("0xE3d7066115f7d6b65F88Dff86288dB4756a7D733");
 
 /// Chain ID for the Pecorino testnet RU chain.
-pub const ROLLUP_ID: u64 = todo!();
+pub const ROLLUP_ID: u64 = 14174;
 /// `Orders` contract address for the Pecorino testnet RU chain.
-pub const RU_ORDERS: Address = todo!();
+pub const RU_ORDERS: Address = address!("0x8e9806fFF56d0660683F0A8157cE70F541A49dD0");
 /// `Passage` contract address for the Pecorino testnet RU chain.
-pub const RU_PASSAGE: Address = todo!();
+pub const RU_PASSAGE: Address = address!("0x862c10E42B7D07dfDE6F74af61B20A55ca5243FE");
 /// Base fee recipient address for the Pecorino testnet RU chain.
-pub const BASE_FEE_RECIPIENT: Address = todo!();
+pub const BASE_FEE_RECIPIENT: Address = address!("0xe0eDA3701D44511ce419344A4CeD30B52c9Ba231");
 
 /// Host system tokens for Pecorino.
 pub const HOST_TOKENS: PredeployTokens =
