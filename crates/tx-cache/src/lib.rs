3a5741a2242fa26b6afe21404af41abbfba9f88e
James <james@prestwi.ch>
2025-05-15 10:52:55 -0400

    fix: add lints to tx-cache

--- crates/tx-cache/src/lib.rs
+++ crates/tx-cache/src/lib.rs
@@ -1,4 +1,21 @@
 //! Signet Transaction Cache types and client.
 
+#![warn(
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs,
+    unreachable_pub,
+    clippy::missing_const_for_fn,
+    rustdoc::all
+)]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+#![deny(unused_must_use, rust_2018_idioms)]
+#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+
+/// The [`TxCache`] client.
 pub mod client;
+
+/// Response types for the [`TxCache`].
+///
+/// [`TxCache`]: crate::client::TxCache
 pub mod types;
