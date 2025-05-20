e14d7f385c85052ddc6f1f3a0d7357c708143017
dylan <dylan@init4.technology>
2025-05-19 18:10:11 -0600

    adds block number to BuiltBlock

--- crates/sim/src/built.rs
+++ crates/sim/src/built.rs
@@ -19,6 +19,8 @@ pub struct BuiltBlock {
     pub(crate) host_fills: Vec<SignedFill>,
     /// Transactions in the block.
     pub(crate) transactions: Vec<TxEnvelope>,
+    /// The block number for the block.
+    pub(crate) block_number: u64,
 
     /// The amount of gas used by the block so far
     pub(crate) gas_used: u64,
@@ -45,10 +47,21 @@ impl BuiltBlock {
         Self {
             host_fills: Vec::new(),
             transactions: Vec::new(),
+            block_number: 0,
             gas_used: 0,
             raw_encoding: OnceLock::new(),
             hash: OnceLock::new(),
         }
+    }
+
+    /// Gets the block number for the block.
+    pub fn block_number(&self) -> u64 {
+        self.block_number
+    }
+
+    /// Sets the block number for the block.
+    pub fn set_block_number(&mut self, block_number: u64) {
+        self.block_number = block_number;
     }
 
     /// Get the amount of gas used by the block.
