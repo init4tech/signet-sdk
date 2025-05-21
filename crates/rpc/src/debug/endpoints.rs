5cc29bf81d93f32bd420054947bd3159afcfc8b5
evalir <e@evalir.xyz>
2025-05-21 15:02:21 +0200

    chore: properly stub methods

--- crates/rpc/src/debug/endpoints.rs
+++ crates/rpc/src/debug/endpoints.rs
@@ -1,7 +1,12 @@
 use crate::{Pnt, RpcCtx};
+use ajj::HandlerCtx;
+use alloy::{eips::BlockNumberOrTag, primitives::B256};
+use reth::rpc::types::trace::geth::GethDebugTracingOptions;
 use reth_node_api::FullNodeComponents;
 
 pub(super) async fn trace_transaction<Host, Signet>(
+    _hctx: HandlerCtx,
+    (_tx_hash, _opts): (B256, GethDebugTracingOptions),
     _ctx: RpcCtx<Host, Signet>,
 ) -> Result<(), String>
 where
@@ -12,6 +17,8 @@ where
 }
 
 pub(super) async fn trace_block_by_number<Host, Signet>(
+    _hctx: HandlerCtx,
+    (_block, _opts): (BlockNumberOrTag, Option<GethDebugTracingOptions>),
     _ctx: RpcCtx<Host, Signet>,
 ) -> Result<(), String>
 where
--- crates/rpc/src/trace/endpoints.rs
+++ crates/rpc/src/trace/endpoints.rs
@@ -1,7 +1,14 @@
 use crate::{Pnt, RpcCtx};
+use ajj::HandlerCtx;
+use alloy::{eips::BlockId, primitives::map::foldhash::HashSet};
+use reth::rpc::types::trace::parity::TraceType;
 use reth_node_api::FullNodeComponents;
 
-pub(super) async fn trace_block<Host, Signet>(_ctx: RpcCtx<Host, Signet>) -> Result<(), String>
+pub(super) async fn trace_block<Host, Signet>(
+    _hctx: HandlerCtx,
+    _block_id: BlockId,
+    _ctx: RpcCtx<Host, Signet>,
+) -> Result<(), String>
 where
     Host: FullNodeComponents,
     Signet: Pnt,
@@ -10,6 +17,8 @@ where
 }
 
 pub(super) async fn trace_replay_block_transactions<Host, Signet>(
+    _hctx: HandlerCtx,
+    (_block_id, _trace_types): (BlockId, HashSet<TraceType>),
     _ctx: RpcCtx<Host, Signet>,
 ) -> Result<(), String>
 where
