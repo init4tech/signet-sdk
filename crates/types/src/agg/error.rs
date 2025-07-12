a179f8afd6ef5391b97fd96b33b5263f0646b232
James <james@prestwi.ch>
2025-07-12 12:41:39 -0400

    test: apparent lack of extraction

--- crates/types/src/agg/error.rs
+++ crates/types/src/agg/error.rs
@@ -3,20 +3,21 @@ use alloy::primitives::{Address, U256};
 /// Error type for market processing.
 #[derive(Debug, Copy, Clone, thiserror::Error, PartialEq, Eq)]
 pub enum MarketError {
-    /// Insufficient balance to settle a trade.
-    #[error("Insufficient balance when taking from context")]
-    InsufficientBalance {
+    /// Insufficient fill to settle a trade.
+    #[error("Insufficient fill when taking from context. Expected {amount} of {asset} from {recipient} on chain {chain_id}")]
+    InsufficientFill {
         /// The chain ID on which the asset is deployed.
         chain_id: u64,
         /// The asset we expected to be in the context.
         asset: Address,
-        /// The recipient account we tried to take from.
+        /// The recipient account for which we tried to decrease the fill
+        /// amount.
         recipient: Address,
-        /// The amount we tried to take.
+        /// The amount by which we tried to decrease the fill.
         amount: U256,
     },
     /// Missing asset in the context.
-    #[error("No recipients of asset when taking from context")]
+    #[error("No fills of asset when taking from context. Expected {asset} on chain {chain_id}")]
     MissingAsset {
         /// The chain ID on which the asset is deployed.
         chain_id: u64,
