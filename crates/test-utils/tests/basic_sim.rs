8c90599329fb1d5d9a24bf92b57bfddd5a08f97d
James <james@prestwi.ch>
2026-01-07 10:45:08 -0500

    test: add test for successive nonces

--- crates/test-utils/tests/basic_sim.rs
+++ crates/test-utils/tests/basic_sim.rs
@@ -1,6 +1,8 @@
 use alloy::{
     consensus::{
-        constants::GWEI_TO_WEI, transaction::SignerRecoverable, Signed, TxEip1559, TxEnvelope,
+        constants::GWEI_TO_WEI,
+        transaction::{Recovered, SignerRecoverable},
+        Signed, TxEip1559, TxEnvelope,
     },
     network::TxSigner,
     primitives::{Address, TxKind, U256},
@@ -13,6 +15,50 @@ use signet_test_utils::{
 };
 use std::time::Instant;
 
+/// Tests the case where multiple transactions from the same
+/// sender with successive nonces are included in the same
+/// simulation batch.
+///
+/// It'll set up 2 transactions from the same sender with
+/// the same nonce, and then 2 more transactions with the next
+/// two nonces. It will then verify that the simulation
+/// produces a block with 3 successful transactions.
+#[tokio::test]
+pub async fn successive_nonces() {
+    let builder = test_sim_env(Instant::now() + std::time::Duration::from_millis(200));
+
+    // Set up 4 sends from the same sender with successive nonces
+    let sender = &TEST_SIGNERS[0];
+    let to = TEST_USERS[1];
+
+    for nonce in 0..4u64 {
+        let tx = signed_send_with_mfpg(
+            sender,
+            to,
+            U256::from(1000),
+            GWEI_TO_WEI as u128 * 10,
+            nonce.saturating_sub(1), // cute little way to duplicate nonce 0
+        )
+        .await;
+        builder.sim_items().add_tx(tx, 0);
+    }
+
+    // Run the simulator
+    let built = builder.build().await;
+
+    assert_eq!(built.transactions().len(), 3);
+
+    // This asserts that the builder has sorted the transactions by nonce
+    assert!(built.transactions().windows(2).all(|w| {
+        let tx1 = w[0].as_eip1559().unwrap().tx().nonce;
+        let tx2 = w[1].as_eip1559().unwrap().tx().nonce;
+        tx1 < tx2
+    }));
+}
+
+/// This test simulates a transaction from each of the test signers,
+/// with escalating priority fees, and asserts that the simulation
+/// orders them correctly by priority fee.
 #[tokio::test]
 pub async fn complex_simulation() {
     let builder = test_sim_env(Instant::now() + std::time::Duration::from_millis(200));
@@ -24,15 +70,16 @@ pub async fn complex_simulation() {
             TEST_USERS[i],
             U256::from(1000),
             (10 - i) as u128 * GWEI_TO_WEI as u128,
+            0,
         )
         .await;
-        builder.sim_items().add_tx(tx.try_into_recovered().unwrap(), 0);
+        builder.sim_items().add_tx(tx, 0);
     }
 
-    // Set up the simulator
+    // Run the simulator
     let built = builder.build().await;
 
-    assert!(!built.transactions().is_empty());
+    assert_eq!(built.transactions().len(), TEST_SIGNERS.len());
 
     // This asserts that the builder has sorted the transactions by priority
     // fee.
@@ -50,9 +97,9 @@ pub async fn complex_simulation() {
 /// Modify an account with a closure and commit the modified account.
 ///
 /// This code is reproduced and modified from trevm
-fn send_with_mfpg(to: Address, value: U256, mpfpg: u128) -> TxEip1559 {
+fn send_with_mfpg(to: Address, value: U256, mpfpg: u128, nonce: u64) -> TxEip1559 {
     TxEip1559 {
-        nonce: 0,
+        nonce,
         gas_limit: 21_000,
         to: TxKind::Call(to),
         value,
@@ -68,9 +115,10 @@ async fn signed_send_with_mfpg<S: TxSigner<Signature>>(
     to: Address,
     value: U256,
     mpfpg: u128,
-) -> TxEnvelope {
-    let mut tx = send_with_mfpg(to, value, mpfpg);
+    nonce: u64,
+) -> Recovered<TxEnvelope> {
+    let mut tx = send_with_mfpg(to, value, mpfpg, nonce);
     let res = from.sign_transaction(&mut tx).await.unwrap();
 
-    Signed::new_unhashed(tx, res).into()
+    TxEnvelope::from(Signed::new_unhashed(tx, res)).try_into_recovered().unwrap()
 }
