0592175114d98a28d8122e5e4f541d4de620d38d
Fraser Hutchison <190532+Fraser999@users.noreply.github.com>
2026-01-27 13:07:32 +0000

    initial scaffolding for signet-orders crate (#169)

---
+++ crates/orders/README.md
@@ -0,0 +1,3 @@
+## signet-orders
+
+This crate contains utilities for placing and filling orders on Signet.
---
+++ crates/orders/src/lib.rs
@@ -0,0 +1,15 @@
+//! Signet Orders Library
+//!
+//! Contains utilities for placing and filling orders on Signet.
+
+#![warn(
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs,
+    unreachable_pub,
+    clippy::missing_const_for_fn,
+    rustdoc::all
+)]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+#![deny(unused_must_use, rust_2018_idioms)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
