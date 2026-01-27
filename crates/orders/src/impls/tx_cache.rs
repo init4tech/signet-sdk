d964b1ec3081484e26f089835a4072d60dd7d992
Fraser Hutchison <190532+Fraser999@users.noreply.github.com>
2026-01-27 17:25:55 +0000

    feat(signet-orders): impl new traits for TxCache

---
+++ crates/orders/src/impls/tx_cache.rs
@@ -0,0 +1,43 @@
+use crate::{BundleSubmitter, OrderSource, OrderSubmitter};
+use futures_util::future::Either;
+use futures_util::stream::{self, Stream, StreamExt};
+use signet_bundle::SignetEthBundle;
+use signet_tx_cache::{types::TxCacheSendBundleResponse, TxCache, TxCacheError};
+use signet_types::SignedOrder;
+
+impl OrderSubmitter for TxCache {
+    type Error = TxCacheError;
+
+    async fn submit_order(&self, order: SignedOrder) -> Result<(), Self::Error> {
+        self.forward_order(order).await
+    }
+}
+
+impl OrderSource for TxCache {
+    type Error = TxCacheError;
+
+    fn get_orders(&self) -> impl Stream<Item = Result<SignedOrder, Self::Error>> + Send {
+        stream::unfold(Some(None), move |cursor| async move {
+            let cursor = cursor?;
+
+            match TxCache::get_orders(self, cursor).await {
+                Ok(response) => {
+                    let (inner, next_cursor) = response.into_parts();
+                    let orders = stream::iter(inner.orders).map(Ok);
+                    Some((Either::Left(orders), Some(next_cursor)))
+                }
+                Err(error) => Some((Either::Right(stream::once(async { Err(error) })), None)),
+            }
+        })
+        .flatten()
+    }
+}
+
+impl BundleSubmitter for TxCache {
+    type Response = TxCacheSendBundleResponse;
+    type Error = TxCacheError;
+
+    async fn submit_bundle(&self, bundle: SignetEthBundle) -> Result<Self::Response, Self::Error> {
+        self.forward_bundle(bundle).await
+    }
+}
